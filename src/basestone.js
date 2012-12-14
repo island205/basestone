91fba8d366c0be8e33bd2a136e24d66998742377
island205 <island205@gmail.com>
2012-12-15 00:03:29 +0800

    README.md added

--- src/basestone.js
+++ src/basestone.js
@@ -1,8 +1,8 @@
-(function(root) {
+(function (root) {
 	var
 	_
 
-	if(typeof exports !== 'undefined') {
+	if (typeof exports !== 'undefined') {
 		_ = require('underscore')
 	} else {
 		_ = root._
@@ -12,59 +12,59 @@
 	 * http://wiki.ecmascript.org/doku.php?id=harmony:simple_maps_and_sets
 	 * _.Map
 	 */
-	var Map = _.Map = function(iterable) {
-			var
-			that = this
-			iterable = iterable || []
-			this._keys = []
-			this._vals = []
-			_.forEach(iterable, function(element) {
-				that.set(element[0], element[1])
-			})
-		}
+	var Map = _.Map = function (iterable) {
+		var
+		that = this
+		iterable = iterable || []
+		this._keys = []
+		this._vals = []
+		_.forEach(iterable, function (element) {
+			that.set(element[0], element[1])
+		})
+	}
 	_.extend(Map.prototype, {
-		get: function(key) {
+		get: function (key) {
 			var i = _.indexOf(this._keys, key)
-			return i < 0 ? undefined : this._vals[i]
+			return i < 0 ? undefined: this._vals[i]
 		},
-		has: function(key) {
+		has: function (key) {
 			return _.indexOf(this._keys, key) >= 0
 		},
-		set: function(key, val) {
+		set: function (key, val) {
 			var keys, i
 			keys = this._keys
 			i = _.indexOf(keys, key)
-			if(i < 0) {
+			if (i < 0) {
 				i = keys.length
 			}
 			keys[i] = key
 			this._vals[i] = val
 		},
-		remove: function(key) {
+		remove: function (key) {
 			var keys, i
 			keys = this._keys
 			i = _.indexOf(keys, key)
-			if(i < 0) {
+			if (i < 0) {
 				return false
 			}
 			keys.splice(i, 1)
 			this._vals.splice(i, 1)
 			return true
 		},
-		items: function(iterator) {
+		items: function (iterator) {
 			var keys = this._keys
-			for(var i = 0; i < keys.length; i++) {
+			for (var i = 0; i < keys.length; i++) {
 				iterator(keys[i], this._vals[i])
 			}
 		},
-		keys: function(iterator) {
+		keys: function (iterator) {
 			var keys = this._keys
-			for(var i = 0; i < keys.length; i++) {
+			for (var i = 0; i < keys.length; i++) {
 				iterator(keys[i])
 			}
 		},
-		values: function(iterator) {
-			for(var i = 0; i < this._keys.length; i++) {
+		values: function (iterator) {
+			for (var i = 0; i < this._keys.length; i++) {
 				iterator(this._vals[i])
 			}
 		}
@@ -74,69 +74,69 @@
 	 * http://wiki.ecmascript.org/doku.php?id=harmony:simple_maps_and_sets
 	 * _.Set
 	 */
-	var Set = _.Set = function(iterable) {
-			var
-			that = this
-			iterable = iterable || []
-			this._map = new _.Map()
-			_.forEach(iterable, function(element) {
-				that.add(element)
-			})
-		}
+	var Set = _.Set = function (iterable) {
+		var
+		that = this
+		iterable = iterable || []
+		this._map = new _.Map()
+		_.forEach(iterable, function (element) {
+			that.add(element)
+		})
+	}
 	_.extend(Set.prototype, {
-		add: function(key) {
+		add: function (key) {
 			this._map.set(key, true)
 		},
-		has: function(key) {
+		has: function (key) {
 			return this._map.has(key)
 		},
-		remove: function(key) {
+		remove: function (key) {
 			return this._map.remove(key)
 		},
-		values: function(iterator) {
+		values: function (iterator) {
 			this._map.keys(iterator)
 		}
 	})
 
-	var Stack = _.Stack = function(iterator) {
-			var
-			that = this
-			iterator = iterator || []
-			this._stack = []
-			_.forEach(iterator, function(element) {
-				that.push(element)
-			})
-		}
+	var Stack = _.Stack = function (iterator) {
+		var
+		that = this
+		iterator = iterator || []
+		this._stack = []
+		_.forEach(iterator, function (element) {
+			that.push(element)
+		})
+	}
 	_.extend(Stack.prototype, {
-		push: function(element) {
+		push: function (element) {
 			this._stack.push(element)
 		},
-		pop: function() {
+		pop: function () {
 			return this._stack.pop()
 		},
-		peek: function() {
+		peek: function () {
 			var stack = this._stack
 			return stack[stack.length]
 		}
 	})
 
-	var Queue = _.Queue = function(iterator) {
-			var
-			that = this
-			iterator = iterator || []
-			this._queue = []
-			_.forEach(iterator, function(element) {
-				that.add(element)
-			})
-		}
+	var Queue = _.Queue = function (iterator) {
+		var
+		that = this
+		iterator = iterator || []
+		this._queue = []
+		_.forEach(iterator, function (element) {
+			that.add(element)
+		})
+	}
 	_.extend(Queue.prototype, {
-		add: function(element) {
+		add: function (element) {
 			this._queue.push(element)
 		},
-		peek: function() {
+		peek: function () {
 			return this._queue[0]
 		},
-		poll: function() {
+		poll: function () {
 			return this._queue.shift()
 		}
 	})
@@ -145,48 +145,48 @@
 	 * http://eloquentjavascript.net/appendix2.html
 	 */
 
-	var BinaryHeap = _.BinaryHeap = function(iterator, scorer) {
-			var
-			that = this
-			iterator = iterator || []
-			this._binaryHeap = []
-			this.scorer = scorer ||
-			function(element) {
-				return element
-			}
-			_.forEach(iterator, function(element) {
-				that.push(element)
-			})
+	var BinaryHeap = _.BinaryHeap = function (iterator, scorer) {
+		var
+		that = this
+		iterator = iterator || []
+		this._binaryHeap = []
+		this.scorer = scorer ||
+		function (element) {
+			return element
 		}
+		_.forEach(iterator, function (element) {
+			that.push(element)
+		})
+	}
 
 	_.extend(BinaryHeap.prototype, {
-		push: function(element) {
+		push: function (element) {
 			var binaryHeap = this._binaryHeap
 			this._binaryHeap.push(element)
 			this.bubbleUp(binaryHeap.length - 1)
 		},
-		pop: function() {
+		pop: function () {
 			var binaryHeap = this._binaryHeap,
-				top = binaryHeap[0],
-				end = binaryHeap.pop()
-				if(binaryHeap.length > 0) {
-					binaryHeap[0] = end
-					this.sinkDown(0)
-				}
+			top = binaryHeap[0],
+			end = binaryHeap.pop()
+			if (binaryHeap.length > 0) {
+				binaryHeap[0] = end
+				this.sinkDown(0)
+			}
 			return top
 		},
-		remove: function(element) {
+		remove: function (element) {
 			var that = this,
-				binaryHeap = this._binaryHeap,
-				length = binaryHeap.length,
-				end, i, node
-			for(i = 0; i < length; i++) {
+			binaryHeap = this._binaryHeap,
+			length = binaryHeap.length,
+			end, i, node
+			for (i = 0; i < length; i++) {
 				node = binaryHeap[i]
-				if(node === element) {
+				if (node === element) {
 					end = binaryHeap.pop()
-					if(i !== length - 1) {
+					if (i !== length - 1) {
 						binaryHeap[i] = end
-						if(end < node) {
+						if (end < node) {
 							that.bubbleUp(i)
 						} else {
 							that.sinkDown(i)
@@ -196,16 +196,17 @@
 				}
 			}
 		},
-		size: function() {
+		size: function () {
 			return this._binaryHeap.length
 		},
-		bubbleUp: function(position) {
+		bubbleUp: function (position) {
 			var element = this._binaryHeap[position],
-				parentN, parent
-			while(position > 0) {
+			parentN,
+			parent
+			while (position > 0) {
 				parentN = Math.floor((position + 1) / 2) - 1
 				parent = this._binaryHeap[parentN]
-				if(this.scorer(element) < this.scorer(parent)) {
+				if (this.scorer(element) < this.scorer(parent)) {
 					this._binaryHeap[parentN] = element
 					this._binaryHeap[position] = parent
 					position = parentN
@@ -214,27 +215,32 @@
 				}
 			}
 		},
-		sinkDown: function(position) {
+		sinkDown: function (position) {
 			var binaryHeap = this._binaryHeap,
-				length = binaryHeap.length,
-				element = binaryHeap[position],
-				leftChildPos, rightChildPos, leftChild, rightChild, swapPos = position
-			while(true) {
-				leftChildPos = position * 2 + 1, rightChildPos = leftChildPos + 1
+			length = binaryHeap.length,
+			element = binaryHeap[position],
+			leftChildPos,
+			rightChildPos,
+			leftChild,
+			rightChild,
+			swapPos = position
+			while (true) {
+				leftChildPos = position * 2 + 1,
+				rightChildPos = leftChildPos + 1
 				swapPos = position
-				if(leftChildPos < length) {
+				if (leftChildPos < length) {
 					leftChild = binaryHeap[leftChildPos]
-					if(this.scorer(leftChild) < this.scorer(element)) {
+					if (this.scorer(leftChild) < this.scorer(element)) {
 						swapPos = leftChildPos
 					}
 				}
-				if(rightChildPos < length) {
+				if (rightChildPos < length) {
 					rightChild = binaryHeap[rightChildPos]
-					if(this.scorer(rightChild) < this.scorer(binaryHeap[swapPos])) {
+					if (this.scorer(rightChild) < this.scorer(binaryHeap[swapPos])) {
 						swapPos = rightChildPos
 					}
 				}
-				if(swapPos !== position) {
+				if (swapPos !== position) {
 					binaryHeap[position] = binaryHeap[swapPos]
 					binaryHeap[swapPos] = element
 					position = swapPos
@@ -247,50 +253,50 @@
 	/**
 	 * http://www.nczonline.net/blog/2009/04/13/computer-science-in-javascript-linked-list/
 	 */
-	var LinkList = _.LinkList = function() {
-			this._length = 0
-			this._head = null
-		}
+	var LinkList = _.LinkList = function () {
+		this._length = 0
+		this._head = null
+	}
 
 	_.extend(LinkList.prototype, {
-		add: function(node) {
+		add: function (node) {
 			var current
 			node = {
 				data: node,
 				next: null
 			}
-			if(this._head === null) {
+			if (this._head === null) {
 				this._head = node
 			} else {
 				current = this._head
-				while(current.next) {
+				while (current.next) {
 					current = current.next
 				}
 				current.next = node
 			}
 			this._length++
 		},
-		get: function(index) {
+		get: function (index) {
 			var current, i = 0
-			if(index < -1 || index > this._length) {
+			if (index < - 1 || index > this._length) {
 				return null
 			}
 			current = this._head
-			while(i++ < index) {
+			while (i++ < index) {
 				current = current.next
 			}
 			return current.data
 		},
-		remove: function(index) {
+		remove: function (index) {
 			var current, previous, i = 0
-			if(index < -1 || index > this._length) {
+			if (index < - 1 || index > this._length) {
 				return null
 			}
 			current = this._head
-			if(index === 0) {
+			if (index === 0) {
 				this._head = current.next
 			} else {
-				while(i++ < index) {
+				while (i++ < index) {
 					previous = current
 					current = current.next
 				}
@@ -299,9 +305,9 @@
 			this._length--
 			return current.data
 		},
-		items: function(iterable) {
+		items: function (iterable) {
 			var current = this._head
-			while(current) {
+			while (current) {
 				iterable(current.data)
 				current = current.next
 			}
@@ -309,10 +315,11 @@
 	})
 }).call(this)
 
-function() {
-	function() {
-		function() {
+function () {
+	function () {
+		function () {
 
 		}
 	}
 }
+
--- test/index.js
+++ test/index.js
@@ -1,83 +1,84 @@
-_ = require("../src/collection")
-assert = require("assert")
-describe("Map", function(){
-    var map= null
-    beforeEach(function(){
-        map = new _.Map
-    })
+var
+_ = require('../src/collection'),
+assert = require('assert')
+describe('Map', function () {
+	var map = null
+	beforeEach(function () {
+		map = new _.Map()
+	})
 
-    it("can set a object key, and can get value by object", function(){
-        map.set(this, "this")
-        assert.equal("this", map.get(this))
-    })
+	it('can set a object key, and can get value by object', function () {
+		map.set(this, 'this')
+		assert.equal('this', map.get(this))
+	})
 
-    it("can delete a key", function(){
-        map.set(this, "this")
-        assert.ok(map["delete"](this))
-        assert.ok(!map["delete"](this))
-        assert.ok(!map["delete"](new Object()))
-    })
+	it('can delete a key', function () {
+		map.set(this, 'this')
+		assert.ok(map['delete'](this))
+		assert.ok(!map['delete'](this))
+		assert.ok(!map['delete']({}))
+	})
 
-    it("can iterate by items or keys or values", function(){
-        var key = new Object(),
-            that = this
-        map.set(this, "this")
-        map.set(key, "key")
-        map.items(function(k, v){
-            assert.ok(that === k || key === k)
-            assert.ok("this" === v || 'key' === v)
-        })
-        map.keys(function(k){
-            assert.ok(that === k || key === k)
-        })
-        map.values(function(v){
-            assert.ok("this" === v || 'key' === v)
-        })
-    })
+	it('can iterate by items or keys or values', function () {
+		var
+        key = {},
+		that = this
+		map.set(this, 'this')
+		map.set(key, 'key')
+		map.items(function (k, v) {
+			assert.ok(that === k || key === k)
+			assert.ok('this' === v || 'key' === v)
+		})
+		map.keys(function (k) {
+			assert.ok(that === k || key === k)
+		})
+		map.values(function (v) {
+			assert.ok('this' === v || 'key' === v)
+		})
+	})
 })
 
-describe("Set", function(){
-    var set = null
-    beforeEach(function(){
-        set = new _.Set
-    })
+describe('Set', function () {
+	var set = null
+	beforeEach(function () {
+		set = new _.Set()
+	})
 
-    it("can add a valye", function(){
-        set.add(this)
-        assert.ok(true)
-    })
+	it('can add a valye', function () {
+		set.add(this)
+		assert.ok(true)
+	})
 
-    it("can test if has some value", function(){
-        set.add(this)
-        assert.ok(set.has(this))
-    })
+	it('can test if has some value', function () {
+		set.add(this)
+		assert.ok(set.has(this))
+	})
 
-    it("can delete value", function(){
-        set.add(this)
-        assert.ok(set.has(this))
-        set["delete"](this)
-        assert.ok(!set.has(this))
-    })
-    it("can iterate it values",function(){
-        var that = this
-        set.add(this)
-        set.add("this")
-        set.values(function(value){
-            assert.ok(value === that || value === "this")
-        })
-    })
+	it('can delete value', function () {
+		set.add(this)
+		assert.ok(set.has(this))
+		set['delete'](this)
+		assert.ok(!set.has(this))
+	})
+	it('can iterate it values', function () {
+		var that = this
+		set.add(this)
+		set.add('this')
+		set.values(function (value) {
+			assert.ok(value === that || value === 'this')
+		})
+	})
 })
 
-describe("Heap", function(){
-    var heap = null
-    beforeEach(function(){
-        heap = new _.Heap
-    })
-    it("can add node to it", function(){
-        heap.add(1)
-        heap.add(2)
-        heap.add(3)
-        assert.deepEqual(heap._tree, [3,1,2])
-    })
+describe('Heap', function () {
+	var heap = null
+	beforeEach(function () {
+		heap = new _.Heap()
+	})
+	it('can add node to it', function () {
+		heap.add(1)
+		heap.add(2)
+		heap.add(3)
+		assert.deepEqual(heap._tree, [3, 1, 2])
+	})
 })
-
