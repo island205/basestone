97036d1039d836f49221f5490bfb6b7fa04af9d8
island205 <island205@gmail.com>
2012-12-17 12:56:55 +0800

    set

---
+++ src/set.js
@@ -0,0 +1,45 @@
+var
+EventEmitter = require('./emitter').EventEmitter,
+Set = require('./structure/set').Set,
+util = require('./util').util,
+UPDATE_METHOD = 'add remove'.split(' ')
+
+function set(iterable) {
+	var
+	st = new Set(iterable)
+
+	function set(key) {
+		var
+		len, type, iterable
+		len = util.makeArray(arguments).length
+		type = typeof key
+
+		if (len === 0) {
+			return st
+		} else {
+			if (type === 'array') {
+				iterable = key
+				iterable.forEach(function (item) {
+					set.add(item)
+				})
+			} else {
+				set.add(key)
+			}
+		}
+	}
+
+	'add has remove values'.split(' ').forEach(function (method) {
+		set[method] = function () {
+			st[method].apply(st, arguments)
+			if (UPDATE_METHOD.indexOf(method) > - 1) {
+				set.emit('update', st)
+			}
+		}
+	})
+
+	util.extend(set, EventEmitter.prototype)
+	return set
+}
+
+exports.set = set
+
